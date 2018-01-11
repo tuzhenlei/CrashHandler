2f70c08c14eb59e23ecd22b40d465c082a88bf3c
tuzhenlei <tuzhenlei@senyint.com>
2018-01-11 18:12:15 +0800

    注释更加友好

--- app/src/main/java/com/demo/crash/MyApplication.java
+++ app/src/main/java/com/demo/crash/MyApplication.java
@@ -16,19 +16,23 @@ public class MyApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		/**
-		 * 不带重启的
+		 * 简单初始化
 		 */
 //		CrashHandler.getInstance().init(this, BuildConfig.DEBUG);
+		
 		/**
-		 * 带重启的
+		 * 个性初始化
+		 */
+		CrashHandler.getInstance().init(this, BuildConfig.DEBUG, true, 0, MainActivity.class);
+		
+		
+		/**
 		 * 参数1:this
 		 * 参数2:是否保存日志到SD卡crash目录，建议设置为BuildConfig.DEBUG，在debug时候保存，方便调试
 		 * 参数3:是否crash后重启APP
 		 * 参数4:多少秒后重启app，建议设为0，因为重启采用闹钟定时任务模式，app会反应3秒钟，所以最快也是3-4秒后重启
 		 * 参数5：重启后打开的第一个activity，建议是splashActivity
 		 */
-
-		CrashHandler.getInstance().init(this, BuildConfig.DEBUG, true, 0, MainActivity.class);
 
 
 		/**
